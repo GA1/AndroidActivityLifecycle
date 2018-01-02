d17b46f7c333eb2499566b4e60da5e8c559051f9
GA1 <nvkga1@gmail.com>
2018-01-02 02:35:28 +0100

    Add MainActivity with all on... methods overriden

---
+++ app/src/main/java/com/ga1/androidactivitylifecycle/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.ga1.androidactivitylifecycle
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+
+class MainActivity : Activity() {
+
+    private val tag = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.article_main)
+        Log.e(tag, "onCreate Call")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(tag, "onStart Call")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(tag, "onResume Call")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(tag, "onPause Call")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(tag, "onStop Call")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(tag, "onDestroy Call")
+    }
+
+    fun goToNextActivity(v: View) {
+
+    }
+
+
+}
